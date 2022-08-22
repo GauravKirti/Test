b8c2eee1c520f8c10784c65e1bb36412a521ce63
Gaurav Kirti <67681298+GauravKirti@users.noreply.github.com>
2022-08-22 11:33:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello, World..!!")
+import requests
+
+def googleSearch(query):
+    with requests.session() as c:
+        url = 'https://www.google.co.in'
+        query = {'q': query}
+        urllink = requests.get(url, params=query)
+        print urllink.url
+
+googleSearch('python')
